ef3544f5d945d6ecacd1e8ab874a676d60f3b3be
LaraLaPierre <larablapierre@gmail.com>
2018-01-22 21:34:19 -0600

    added migrations

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,4 @@
-class ContactsController < ApplicationController
+ class ContactsController < ApplicationController
   def index 
     contacts = Contact.all
     render json: contacts.as_json
@@ -8,8 +8,10 @@ class ContactsController < ApplicationController
     contact = Contact.new(
                           first_name: params[:first_name],
                           last_name: params[:last_name],
+                          middle_name: params[:middle_name],
                           email: params[:email],
-                          phone_number: params[:phone_number]
+                          phone_number: params[:phone_number],
+                          bio: params[:bio]
                           )
 
     contact.save
@@ -25,9 +27,11 @@ class ContactsController < ApplicationController
     contact = Contact.find(params[:id])
 
     contact.first_name = params[:first_name] || contact.first_name
+    contact.middle_name = params[:middle_name] || contact.middle_name
     contact.last_name = params[:last_name] || contact.last_name
     contact.email = params[:email] || contact.email
     contact.phone_number = params[:phone_number] || contact.phone_number
+    contact.bio = params[:bio] || contact.bio
 
     contact.save
     render json: contact.as_json
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,22 +1,24 @@
 class Contact < ApplicationRecord
-  def as_json
-    {
-      id: id,
-      first_name: first_name,
-      last_name: last_name,
-      full_name: full_name,
-      email: email,
-      phone_number: phone_number,
-      updated_at: friendly_updated_at,
-    }
-  end
-
   def friendly_updated_at
     updated_at.strftime("%b %d, %Y")
   end 
 
   def full_name
-    "#{first_name} #{last_name}"
+    "#{first_name} #{middle_name} #{last_name}"
   end
+
+  def as_json
+    {
+      id: id,
+      first_name: first_name,
+      middle_name: middle_name,
+      last_name: last_name,
+      full_name: full_name,
+      email: email,
+      phone_number: phone_number,
+      bio: bio,
+      updated_at: friendly_updated_at
+    }
+  end 
    
 end
