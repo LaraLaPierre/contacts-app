a3be3eecfe48744c971400042a7f8d2e52c7f53f
LaraLaPierre <larablapierre@gmail.com>
2018-01-23 20:12:52 -0600

    added validations

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -14,8 +14,11 @@
                           bio: params[:bio]
                           )
 
-    contact.save
-    render json: contact.as_json
+    if contact.save
+      render json: contact.as_json
+    else 
+      render json: {errors: contact.errors.full_messages}, status: :unprocessable_entity
+    end 
   end 
 
   def show 
@@ -33,8 +36,11 @@
     contact.phone_number = params[:phone_number] || contact.phone_number
     contact.bio = params[:bio] || contact.bio
 
-    contact.save
-    render json: contact.as_json
+    if contact.save
+      render json: contact.as_json
+    else 
+      render json: {errors: contact.errors.full_messages}, status: :unprocessable_entity
+    end 
   end 
 
   def destroy
